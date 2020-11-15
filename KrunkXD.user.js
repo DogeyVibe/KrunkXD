8e42ddcac1db16065481aac19e92b26e6d76e08c
DogeyVibe <74507702+DogeyVibe@users.noreply.github.com>
2020-11-15 10:34:22 -0800

    Create KrunkXD.user.js

---
+++ KrunkXD.user.js
@@ -0,0 +1,133 @@
+// ==UserScript==
+// @name         KrunkXD
+// @version      1.0.1
+// @author       Dogey
+// @description  KrunkXD - op kranker dot eye oh h4x dll firefox api
+// @match        *://krunker.io/*
+// @grant        GM.registerMenuCommand
+// @grant        unsafeWindow
+// ==/UserScript==
+
+window = unsafeWindow;
+
+window.hackToggle = true;
+
+let game, input, me, recon, lock;
+
+const key = {
+    frame: 0,
+    delta: 1,
+    ydir: 2,
+    xdir: 3,
+    moveDir: 4,
+    shoot: 5,
+    scope: 6,
+    jump: 7,
+    crouch: 8,
+    reload: 9,
+    weaponScroll: 10,
+    weaponSwap: 11,
+    moveLock: 12
+};
+
+const varsRe = {
+    cnBSeen: { regex: /if\(!\w+\['(\w+)'\]\)continue;/, pos: 1 },
+    recoilAnimY: { regex: /,this\['(\w+)']\+=this\['recoilForce']/, pos: 1 },
+    aimVal: { regex: /this\['(\w+)']-=0x1\/\(this\['weapon']\['\w+']\/\w+\)/, pos: 1 },
+    pchObjc: { regex: /0x0,this\['(\w+)']=new \w+\['Object3D']\(\),this/, pos: 1 },
+    didShoot: { regex: /--,\w+\['(\w+)']=!0x0/, pos: 1 },
+    nAuto: { regex: /'Single\\x20Fire','varN':'(\w+)'/, pos: 1 }
+}
+
+const vars = {}
+
+const getDistance = function(x1, y1, z1, x2, y2, z2) {
+    var dx = x1 - x2;
+    var dy = y1 - y2;
+    var dz = z1 - z2;
+    return Math.hypot(dx, dy, dz);
+}
+
+const getDirection = function(a, b, c, d) {
+    return Math.atan2(b - d, a - c);
+}
+
+const getXDir = function(e, n, r, i, a, s) {
+    const o = Math.abs(n - a);
+    const dis = getDistance(e, n, r, i, a, s);
+    return Math.asin(o / dis) * (n > a ? -1 : 1);
+}
+
+const getTarget = function() {
+    let fil = game.players.list.filter(function(player) {
+        return (player[vars.cnBSeen] && player.active && player !== me) && (!me.team || me.team !== player.team)
+    });
+    return fil[0];
+}
+
+const shoot = function() {
+    input[key.scope] = 1;
+    let weapon = me.weapon;
+    if (weapon[vars.nAuto] && me[vars.didShoot]) {
+        input[key.shoot] = 0;
+    } else if (!me[vars.aimVal]) {
+        input[key.shoot] = 1;
+    }
+}
+
+const toggleHack = function() {
+    window.hackToggle = !window.hackToggle;
+}
+
+window.useHack = function(i, g, m, r, l) {
+    game = g; input = i; me = m; recon = r; lock = l;
+
+    let target = getTarget();
+    if (target && window.hackToggle) {
+        const yDire = (getDirection(me.z, me.x, target.z, target.x) || 0) * 1000;
+        const xDire = ((getXDir(me.x, me.y, me.z, target.x, target.y, target.z) || 0) - (0.3 * me[vars.recoilAnimY])) * 1000;
+
+        input[key.ydir] = yDire;
+        input[key.xdir] = xDire;
+
+        shoot();
+    }
+}
+
+const patchCode = function(code) {
+    for (let i in varsRe) {
+        const key = varsRe[i];
+        vars[i] = key.regex.exec(code)[key.pos];
+    }
+    code = code.replace(varsRe.cnBSeen.regex, "if (!window.hackToggle) { $& };").replace(/!(\w+)\['transparent']/, `$&& (!window.hackToggle || !$1.penetrable )`).replace(/(this\['\w+']=function\(\w+,\w+,\w+,\w+\){)(this\['recon'])/, "$1{\nconst [input, game, recon, lock] = arguments, me = this;\nwindow.useHack(input, game, me, recon, lock);};$2") // patches code
+    GM.registerMenuCommand("Toggle Hack", toggleHack);
+    return code;
+}
+
+// <start> Hook by hrt/ttap (but I modified a little)
+function hookJoin(wnd) {
+    const arrayJoin = wnd.Array.prototype.join;
+    wnd.Array.prototype.join = new Proxy(arrayJoin, {
+        apply: function(target, _this, _arguments) {
+            let ret = Function.prototype.apply.apply(target, [_this, _arguments]);
+                if (_arguments.length && _arguments[0] == "" && _this.length > (420 * 69 /* funny numbers */)) {
+                    let code = ret;
+                    code = patchCode(code);
+                    return code;
+                }
+                return ret;
+            }
+        });
+}
+
+const appendChild = HTMLBodyElement.prototype.appendChild;
+HTMLBodyElement.prototype.appendChild = new Proxy(appendChild, {
+    apply: function(target, _this, _arguments) {
+        let ret = Function.prototype.apply.apply(target, [_this, _arguments]);
+        if (_arguments.length && _arguments[0].__proto__ == HTMLIFrameElement.prototype) {
+            hookJoin(_arguments[0].contentWindow);
+        }
+        return ret;
+    }
+});
+// <end> Hook by hrt/ttap (but I modified a little)
